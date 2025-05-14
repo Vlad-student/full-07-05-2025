e62727a25ac89ea83615c7851bff587713d39725
Vlad-student <vlad.v.terletskiy@gmail.com>
2025-05-14 17:52:48 +0300

    Fisx add image to create sport

--- client/src/validation/sport.validate.js
+++ client/src/validation/sport.validate.js
@@ -1,4 +1,5 @@
 import * as Yup from "yup";
+import CONSTANTS from "../constants";
 
 export const createValidationShema = Yup.object({
   name: Yup.string()
@@ -6,4 +7,11 @@ export const createValidationShema = Yup.object({
     .matches(/^[A-z][a-z]{2,63}$/, "Upper first letter")
     .required(),
   isOlimpic: Yup.boolean(),
+  image: Yup.mixed()
+    .test("fileSize", "Size must be less 5MB", (value) => {
+      return !value || value.size <= CONSTANTS.MAX_FILE_SIZE;
+    })
+    .test("fileType", "File not available", (value) => {
+      return !value || CONSTANTS.FILE_TYPES.includes(value.type);
+    }),
 });
